01ee3b199626d7d676aec7c2924eac7c175f4e68
dufeq <jakubiak99@gmail.com>
2023-05-20 13:37:52 +0200

    First Commit

---
+++ src/main/java/sri05/soap/Sri05SoapApplication.java
@@ -0,0 +1,13 @@
+package sri05.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sri05SoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sri05SoapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sri05/soap/Sri05SoapApplicationTests.java
@@ -0,0 +1,13 @@
+package sri05.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sri05SoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
